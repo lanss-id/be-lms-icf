8c721ba06e58dc3ebc9d6971f6d90182404b9064
lanss-id <maulanakayyis354@gmail.com>
2024-12-10 20:18:35 +0700

    fix: use nest default service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
-import { PrismaService } from './prisma.service';
+// import { PrismaService } from './prisma.service';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [PrismaService],
+  providers: [AppService],
 })
 export class AppModule {}
